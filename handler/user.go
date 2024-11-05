b9a792ef3bcaa24a73e7e9a9c8550ac2a819af4f
myunus <yunuslon97@gmail.com>
2024-11-06 00:14:39 +0700

    feat: format validator

---
+++ handler/user.go
@@ -0,0 +1,44 @@
+package handler
+
+import (
+	"net/http"
+	"startup/helper"
+	"startup/user"
+
+	"github.com/gin-gonic/gin"
+)
+
+type userHandler struct {
+	userService user.Service
+}
+
+func NewUserHandler(userService user.Service) *userHandler {
+	return &userHandler{userService}
+}
+
+func (h *userHandler) RegisterUser(c *gin.Context) {
+	var input user.RegisterUserInput
+
+	err := c.ShouldBindJSON(&input)
+	if err != nil {
+		errors := helper.FormatValidationError(err)
+		errorMessage := gin.H{"errors": errors}
+
+		response := helper.APIResponse("Register Account failed", http.StatusUnprocessableEntity, "error", errorMessage)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	newUser, err := h.userService.RegisterUserInput(input)
+	if err != nil {
+		response := helper.APIResponse("Register Account failed", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	user := user.FormatUser(newUser, "TokenTOkenDummy")
+
+	response := helper.APIResponse("Account has been registered", http.StatusOK, "success", user)
+	c.JSON(http.StatusOK, response)
+
+}
