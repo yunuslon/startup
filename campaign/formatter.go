1c8297fd4272bf4f6ea60a1af4134a1b505fee26
myunus <yunuslon97@gmail.com>
2024-11-14 00:39:38 +0700

    feat: add campaign formatting functions to structure campaign data for API responses

--- campaign/formatter.go
+++ campaign/formatter.go
@@ -1,1 +1,39 @@
 package campaign
+
+type CampaignFormatrer struct {
+	ID               int    `json:"id"`
+	UserID           int    `json:"user_id"`
+	Name             string `json:"name"`
+	ShortDescription string `json:"short_description"`
+	ImageUrl         string `json:"image_url"`
+	GoalAmount       int    `json:"goal_amount"`
+	CurrentAmount    int    `json:"current_amount"`
+}
+
+func FormatCampaign(campaign Campaign) CampaignFormatrer {
+	campaignFormatter := CampaignFormatrer{}
+	campaignFormatter.ID = campaign.ID
+	campaignFormatter.UserID = campaign.UserID
+	campaignFormatter.Name = campaign.Name
+	campaignFormatter.ShortDescription = campaign.ShortDescription
+	campaignFormatter.GoalAmount = campaign.GoalAmount
+	campaignFormatter.CurrentAmount = campaign.CurrentAmount
+	campaignFormatter.ImageUrl = ""
+
+	if len(campaign.CampaignImages) > 0 {
+		campaignFormatter.ImageUrl = campaign.CampaignImages[0].FileName
+	}
+
+	return campaignFormatter
+}
+
+func FormatCampaigns(campaigns []Campaign) []CampaignFormatrer {
+	tmp := []CampaignFormatrer{}
+
+	for _, campaign := range campaigns {
+		campaignFormatter := FormatCampaign(campaign)
+		tmp = append(tmp, campaignFormatter)
+	}
+
+	return tmp
+}
